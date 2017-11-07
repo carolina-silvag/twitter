cd1ccaf47e4eddd26c10c2959e1e79b93ce8249f
carolina-silvag <carolinasguzman@gmail.com>
2017-11-06 23:56:15 -0300

    version 0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,36 @@
+function enabled() {
+	var comments = document.getElementById('comment').value;
+	if (comments.length == 0){
+		document.getElementById("btn").disabled = true;
+		document.getElementById("btn").className = "btn-disabled";
+	}else{
+		document.getElementById("btn").disabled = false;
+		document.getElementById("btn").className = "btn-enabled";
+	}
+}
 function add(){
 	/*tomo texto de textareaa*/
 	var comments= document.getElementById('comment').value;
-	if (comments.length >0) {
-		/*limpealo del textarea*/
-		document.getElementById('comment').value = '';
-		/*el div en donde estan los comentario el html*/
-		var newComments= document.createElement('div');
-		var cont = document.getElementById('cont');
+	/*limpealo del textarea*/
+	document.getElementById('comment').value = '';
+	/*el div en donde estan los comentario el html*/
+	var newComments= document.createElement('div');
+	var cont = document.getElementById('cont');
 
-
-		var paragraph= document.createElement('p');
-		/*paragraph.classList.add('color');*/
-		var nodoText=document.createTextNode(comments);
-		paragraph.appendChild(nodoText);
-
-		/*agregar todos los huerfanos al padre(contenedor)*/
-		newComments.appendChild(paragraph);
-		cont.appendChild(newComments);
-		
-	}else{
-		alert('Debe ingresar un texto');
-	}
+	var paragraph= document.createElement('p');
+	var nodoText=document.createTextNode(comments);
+	paragraph.appendChild(nodoText);
+	/*agregar todos los huerfanos al padre(contenedor)*/
+	newComments.appendChild(paragraph);
+	cont.appendChild(newComments);
+	/*volver a preguntar por cada comentario*/
+	enabled();
 }
+
+function enabled(){
+	var comments= document.getElementById('comment').value;
+	var cantidad = 140 - comments.length
+	document.getElementById('p1').innerHTML=cantidad
+}
+
+
