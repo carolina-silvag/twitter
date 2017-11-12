1533028ed2b1d05a1a099d7b96a17c983b30a31a
carolina-silvag <carolinasguzman@gmail.com>
2017-11-11 22:42:39 -0300

    todo terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
 function enabled() {
 	var comments = document.getElementById('comment').value;
 	var cantidad = 140 - comments.length;
+	var espacios = comments.split(' ').length - 1;
+	var enters = comments.split('\n').length - 1;
 	document.getElementById('p1').innerHTML=cantidad;
-	if (comments.length == 0 || cantidad < 0){
-		document.getElementById("btn").disabled = true;
-		document.getElementById("btn").className = "btn-disabled";
+
+	if (comments.length == 0 || cantidad < 0 || espacios + enters == comments.length){
+		document.getElementById('btn').disabled = true;
+		document.getElementById('btn').className = 'btn-disabled';
 	}else{
-		document.getElementById("btn").disabled = false;
-		document.getElementById("btn").className = "btn-enabled";
+		document.getElementById('btn').disabled = false;
+		document.getElementById('btn').className = 'btn-enabled';
 	}
 }
 function add(){
@@ -18,12 +21,15 @@ function add(){
 	/*el div en donde estan los comentario el html*/
 	var newComments= document.createElement('div');
 	var cont = document.getElementById('cont');
+	var hora =moment().format('LT');
 
 	var paragraph= document.createElement('p');
 	var nodoText=document.createTextNode(comments);
+	var nodoHour=document.createTextNode(hora);
 	paragraph.appendChild(nodoText);
 	/*agregar todos los huerfanos al padre(contenedor)*/
 	newComments.appendChild(paragraph);
+	newComments.appendChild(nodoHour);
 	cont.appendChild(newComments);
 	/*volver a preguntar por cada comentario*/
 	enabled();
@@ -32,20 +38,24 @@ function numColor() {
 	var comments = document.getElementById('comment').value;
 	var cantidad = 140 - comments.length;
 	if (cantidad < 20 && cantidad >=10){
-		document.getElementById("tell").className = "tell-2";
+		document.getElementById('tell').className = 'tell-2';
 	}else if(cantidad < 10 && cantidad >=0){
-		document.getElementById("tell").className = "tell-3";
+		document.getElementById('tell').className = 'tell-3';
 	}else{
-		document.getElementById("tell").className = "tell-1";
+		document.getElementById('tell').className = 'tell-1';
 	}
 }
 
 function agrandar(){
 	var comments = document.getElementById('comment').value;
 	var linea = comments.split('\n');
-		if(linea.length - 1 >= 3){
-			contar+=1;
-			document.getElementById('comment').rows = linea.length - 1;
+	var lineas_necesarias = parseInt(comments.length/37) + 1;
+		if((linea.length - 1 + lineas_necesarias) > 3){
+			document.getElementById('comment').rows = linea.length - 1 + lineas_necesarias;
+		} else {
+			document.getElementById('comment').rows = 3;	
 		}
 }
+
+
 
